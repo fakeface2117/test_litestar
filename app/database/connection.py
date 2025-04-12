7d2a48b121fefaab5b0e39bf50e15d5d8620e3ea
pdnlk <facebal900@mail.ru>
2025-04-12 21:41:05 +0300

    refactor: больше натроек для sessionmaker + добавлен метод инициализации БД

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,12 +1,24 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 from app.core.config import settings
+from app.database.base import Base
 
 engine = create_async_engine(settings.DB_CONNECTION_STRING)
 
-async_session_maker = async_sessionmaker(bind=engine)
+async_session_maker = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+    autocommit=False,
+    autoflush=False
+)
 
 
 async def get_async_session() -> AsyncSession:
     async with async_session_maker() as session:
         yield session
+
+
+async def create_db() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
