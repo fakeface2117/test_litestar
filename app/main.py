5689738bc5f833565b50e92ad967d51bc0287a6f
pdnlk <facebal900@mail.ru>
2025-04-12 21:42:28 +0300

    refactor: переделал инит для БД

--- app/main.py
+++ app/main.py
@@ -1,54 +1,35 @@
 from contextlib import asynccontextmanager
 
 import uvicorn
-from litestar import Litestar, get
+from litestar import Litestar
 from litestar.datastructures import State
-from litestar.plugins.sqlalchemy import (
-    AsyncSessionConfig,
-    SQLAlchemyAsyncConfig,
-    SQLAlchemyInitPlugin,
-    base
-)
 
-from app.api.v1.users_controller import base_router
+from app.api.v1.base_router import v1_router
 from app.core.config import settings
+from app.database.connection import create_db
 
 
 @asynccontextmanager
 async def lifespan(_app: Litestar):
     print('Start app')
-    print(f'http://{settings.DB_HOST}:{settings.DB_PORT}/someapp/schema/swagger')
+    print(settings.SERVICE_SWAGGER_URL)
     _app.state.some_value1 = 'value1'  # передача параметра в стейт
     yield
     print('Stop app')
 
 
-session_config = AsyncSessionConfig(expire_on_commit=False)
-sqlalchemy_config = SQLAlchemyAsyncConfig(
-    connection_string=settings.DB_CONNECTION_STRING, session_config=session_config
-)  # Create 'db_session' dependency.
-sqlalchemy_plugin = SQLAlchemyInitPlugin(config=sqlalchemy_config)
-
-
 async def on_startup() -> None:
     """Initializes the database."""
-    async with sqlalchemy_config.get_engine().begin() as conn:
-        await conn.run_sync(base.UUIDBase.metadata.create_all)
-
-
-@get('/')
-async def index() -> str:
-    return 'Hello World!'
+    await create_db()
 
 
 app = Litestar(
-    route_handlers=[index, base_router],
-    path='/someapp',
-    on_startup=[on_startup],
+    route_handlers=[v1_router],
+    path=settings.SERVICE_BASE_URL,
     lifespan=[lifespan],
-    plugins=[SQLAlchemyInitPlugin(config=sqlalchemy_config)],
+    on_startup=[on_startup],
     state=State({'some_value2': 'value2'}, deep_copy=True)
 )
 
 if __name__ == '__main__':
-    uvicorn.run(app, host=settings.DB_HOST, port=settings.DB_PORT)
+    uvicorn.run(app, host=settings.SERVICE_LOCAL_HOST, port=settings.SERVICE_LOCAL_PORT)
