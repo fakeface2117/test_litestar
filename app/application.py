2dba71ea5ec6ed48c62a6029f8a0fa185ccb8a35
pdnlk <facebal900@mail.ru>
2025-04-13 20:57:34 +0300

    refactor: инициализация приложения в отдельном файле

--- app/application.py
+++ app/application.py
@@ -1,3 +1,32 @@
-class ApplicationCustomizer:
-    # TODO перенести инициализацию и настройку приложения сюда
-    pass
+from contextlib import asynccontextmanager
+
+from litestar import Litestar
+from litestar.datastructures import State
+
+from app.api.v1.base_router import v1_router
+from app.core.config import settings
+from app.database.connection import create_db
+
+
+@asynccontextmanager
+async def lifespan(_app: Litestar):
+    print('Start app')
+    print(settings.SERVICE_SWAGGER_URL)
+    _app.state.some_value1 = 'value1'  # передача параметра в стейт
+    yield
+    print('Stop app')
+
+
+async def on_startup() -> None:
+    """Initializes the database."""
+    await create_db()
+
+
+def create_app() -> Litestar:
+    return Litestar(
+        route_handlers=[v1_router],
+        path=settings.SERVICE_BASE_URL,
+        lifespan=[lifespan],
+        on_startup=[on_startup],
+        state=State({'some_value2': 'value2'}, deep_copy=True)
+    )
--- app/main.py
+++ app/main.py
@@ -1,35 +1,9 @@
-from contextlib import asynccontextmanager
-
 import uvicorn
-from litestar import Litestar
-from litestar.datastructures import State
 
-from app.api.v1.base_router import v1_router
+from app.application import create_app
 from app.core.config import settings
-from app.database.connection import create_db
 
-
-@asynccontextmanager
-async def lifespan(_app: Litestar):
-    print('Start app')
-    print(settings.SERVICE_SWAGGER_URL)
-    _app.state.some_value1 = 'value1'  # передача параметра в стейт
-    yield
-    print('Stop app')
-
-
-async def on_startup() -> None:
-    """Initializes the database."""
-    await create_db()
-
-
-app = Litestar(
-    route_handlers=[v1_router],
-    path=settings.SERVICE_BASE_URL,
-    lifespan=[lifespan],
-    on_startup=[on_startup],
-    state=State({'some_value2': 'value2'}, deep_copy=True)
-)
+app = create_app()
 
 if __name__ == '__main__':
     uvicorn.run(app, host=settings.SERVICE_LOCAL_HOST, port=settings.SERVICE_LOCAL_PORT)
