5b558154e0c6e97e8f4b1ab91cc77632ce51967b
pdnlk <facebal900@mail.ru>
2025-04-16 00:38:20 +0300

    feat: логи для uvicorn

--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ import uvicorn
 
 from app.application import create_app
 from app.core.config import settings
+from app.core.custom_logger import LOGGING_CONFIG
 
 app = create_app()
 
 if __name__ == '__main__':
-    uvicorn.run(app, host=settings.SERVICE_LOCAL_HOST, port=settings.SERVICE_LOCAL_PORT)
+    uvicorn.run(app, host=settings.SERVICE_LOCAL_HOST, port=settings.SERVICE_LOCAL_PORT, log_config=LOGGING_CONFIG)
