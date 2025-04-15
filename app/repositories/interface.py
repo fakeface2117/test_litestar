7a4d27629b79e53d69a13795880b56dabb5cf608
pdnlk <facebal900@mail.ru>
2025-04-16 00:37:24 +0300

    refactor: убрал try except в репе

--- app/repositories/interface.py
+++ app/repositories/interface.py
@@ -1,13 +1,23 @@
+from abc import ABC, abstractmethod
 from typing import TypeVar, Generic
 from uuid import UUID
 
 from sqlalchemy import select, update
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.base import Base
 
 model_table = TypeVar('model_table', bound=Base)
+
+
+class AbstractRepository(ABC):
+    @abstractmethod
+    async def add(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_all(self):
+        raise NotImplementedError
 
 
 class BaseRepository(Generic[model_table]):
@@ -21,11 +31,6 @@ class BaseRepository(Generic[model_table]):
     async def add(self, values: dict):
         new_object = self.ModelT(**values)
         self.async_session.add(new_object)
-        try:
-            await self.async_session.commit()
-        except SQLAlchemyError as sae:
-            await self.async_session.rollback()
-            raise sae
         return new_object
 
     async def get_all(self):
@@ -41,21 +46,11 @@ class BaseRepository(Generic[model_table]):
     async def update_by_id(self, id_: UUID | int, values: dict):
         query = update(self.ModelT).where(self.ModelT.id == id_).values(**values).returning(self.ModelT)
         result = await self.async_session.execute(query)
-        try:
-            await self.async_session.commit()
-        except SQLAlchemyError as sae:
-            await self.async_session.rollback()
-            raise sae
         return result.scalars().first()
 
     async def delete_one_by_id(self, id_: UUID | int):
         data = await self.get_by_id(id_)
         if data:
             await self.async_session.delete(data)
-            try:
-                await self.async_session.commit()
-                return True
-            except SQLAlchemyError as sae:
-                await self.async_session.rollback()
-                raise sae
+            return True
         return False
