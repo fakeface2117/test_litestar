c4d7fb40d426152a9071b175239de2ffb586827b
pdnlk <facebal900@mail.ru>
2025-04-17 00:44:43 +0300

    refactor: сделал uow атрибутом экземпляра в сервисе пользователей

--- app/services/users_service.py
+++ app/services/users_service.py
@@ -2,46 +2,51 @@ from uuid import UUID
 
 from app.api.v1.users.rest_models import UserCreate, User, AllUsers, UserUpdate
 from app.core.custom_logger import logger
-from app.database.unit_of_work import AbstractUnitOfWork
-from app.exceptions.exceptions import UserNotFoundException
+from app.database.unit_of_work import AbstractUnitOfWork, UnitOfWork
+from app.exceptions.exceptions import UserNotFoundException, UsersNotFoundException
 
 
 class UsersService:
     """Класс бизнес логики для пользователя"""
+    def __init__(self, uow: AbstractUnitOfWork):
+        self.uow = uow
 
-    async def create_user(self, uow: AbstractUnitOfWork, user: UserCreate) -> User:
-        async with uow:
-            created_user = await uow.users.add(user.model_dump(exclude_unset=True))
-            await uow.commit()
+    async def create_user(self, user: UserCreate) -> User:
+        async with self.uow:
+            created_user = await self.uow.users.add(user.model_dump(exclude_unset=True))
+            await self.uow.commit()
         return User.model_validate(created_user)
 
-    async def get_user(self, uow: AbstractUnitOfWork, user_id: UUID) -> User:
-        async with uow:
-            user = await uow.users.get_by_id(user_id)
+    async def get_user(self, user_id: UUID) -> User:
+        async with self.uow:
+            user = await self.uow.users.get_by_id(user_id)
             if not user:
                 logger.warning(f'User with id {user_id} not found in database')
                 raise UserNotFoundException(user_id)
             return User.model_validate(user)
 
-    async def get_all_users(self, uow: AbstractUnitOfWork,) -> AllUsers:
-        async with uow:
-            all_users = await uow.users.get_all()
+    async def get_all_users(self) -> AllUsers:
+        async with self.uow:
+            all_users = await self.uow.users.get_all()
+            if not all_users:
+                raise UsersNotFoundException
             return AllUsers(users=[User.model_validate(user) for user in all_users])
 
-    async def update_user_by_id(self, uow: AbstractUnitOfWork, user_id: UUID, user: UserUpdate) -> User:
-        async with uow:
-            updated_user = await uow.users.update_by_id(user_id, user.model_dump(exclude_none=True))
-            await uow.commit()
+    async def update_user_by_id(self, user_id: UUID, user: UserUpdate) -> User:
+        async with self.uow:
+            updated_user = await self.uow.users.update_by_id(user_id, user.model_dump(exclude_none=True))
+            await self.uow.commit()
             return User.model_validate(updated_user)
 
-    async def delete_user(self, uow: AbstractUnitOfWork, user_id: UUID) -> None:
-        async with uow:
-            delete_result = await uow.users.delete_one_by_id(user_id)
+    async def delete_user(self, user_id: UUID) -> None:
+        async with self.uow:
+            delete_result = await self.uow.users.delete_one_by_id(user_id)
             if not delete_result:
                 logger.warning(f'User with id {user_id} not found in database')
                 raise UserNotFoundException(user_id)
-            await uow.commit()
+            await self.uow.commit()
 
 
 async def get_users_service() -> UsersService:
-    return UsersService()
+    uow = UnitOfWork()
+    return UsersService(uow)
