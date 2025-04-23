4957916526d5d8708a8a49179811c4263efbadad
pdnlk <facebal900@mail.ru>
2025-04-24 00:13:33 +0300

    feat: ошибки по машинам

--- app/exceptions/exceptions.py
+++ app/exceptions/exceptions.py
@@ -15,3 +15,21 @@ class UserNotFoundException(NotFoundException):
     def __init__(self, user_id):
         self.user_id = user_id
         super().__init__(f'User with id {user_id} not found in db')
+
+
+class UserCarsNotFoundException(NotFoundException):
+    def __init__(self, user_id):
+        self.user_id = user_id
+        super().__init__(f'Cars for user {user_id} not found in db')
+
+
+class CarNotFoundException(NotFoundException):
+    def __init__(self, car_id, user_id):
+        self.user_id = user_id
+        self.car_id = car_id
+        super().__init__(f'Car with id {car_id} not found in database for user {user_id}')
+
+
+class CarsNotFoundException(NotFoundException):
+    def __init__(self):
+        super().__init__('Cars not found')
