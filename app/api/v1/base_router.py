6060dae6ec4d4f965fdfddae4fc1734450d5386e
pdnlk <facebal900@mail.ru>
2025-04-24 00:15:57 +0300

    feat: добавил контроллер авто в роутер v1

--- app/api/v1/base_router.py
+++ app/api/v1/base_router.py
@@ -1,8 +1,9 @@
 from litestar import Router
 
+from app.api.v1.cars.cars_controller import CarsController
 from app.api.v1.users.users_controller import UserController, OtherController
 
 v1_router = Router(
-    route_handlers=[UserController, OtherController],
+    route_handlers=[UserController, OtherController, CarsController],
     path="/api/v1"
 )
